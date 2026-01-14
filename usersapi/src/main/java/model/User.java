8a0678b54ec7aec876cd0ec27168a25aca0fa738
Gabriel <gabrieldejesus01.gdj@gmail.com>
2026-01-13 22:52:25 -0300

    feat: criando o arquivo user

--- usersapi/src/main/java/model/User.java
+++ usersapi/src/main/java/model/User.java
@@ -1,4 +1,29 @@
 package model;
 
-public class usersModel {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class User {
+    @Entity
+    @Table(name = "users")
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
 }
