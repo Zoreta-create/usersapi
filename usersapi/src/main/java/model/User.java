10e384e2426f95928cd2869e8234b07f88912cbf
Gabriel <gabrieldejesus01.gdj@gmail.com>
2026-01-15 22:36:09 -0300

    feat: corrigindo as anotações

--- usersapi/src/main/java/model/User.java
+++ usersapi/src/main/java/model/User.java
@@ -5,13 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-public class User {
     @Entity
     @Table(name = "users")
-
     @NoArgsConstructor
     @AllArgsConstructor
     @Data
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
